d47b3dbc85f749190bf62414dccbae84fe844ce7
checillia_abigail <checilliaabigail@gmail.com>
2026-07-04 19:37:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// ============================================================
+// KALKULATOR CUAN SHOPEE - SCRIPT.JS
+// Cuan Berkah Digital
+// ============================================================
+
 const DATA_PATH = 'data/';
 const MAX_LIVE_XTRA = 20000;
 
